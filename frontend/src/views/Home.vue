6ba4fedcb480e7b51bc84ed3283f8bd6b8ee172c
TECNOSYLVA\fgarcia <fernando.garcia@xeridia.com>
2017-01-09 12:09:06 +0100

    Switched from plain html to json foreachs.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,43 +1,53 @@
 <template lang="pug">
   md-card(md-with-hover)
-    md-card-media-cover(md-solid)
-      md-card-media(md-ratio='16:9')
-        img(src='/dist/img/harrypotter.jpg')
+    md-card-media-cover(md-solid, v-for="card in cards")
+      md-card-media(md-ratio="16:9")
+        img(v-bind:src="'/dist/img/' + card.media")
       md-card-area
         md-card-header
-          .md-title Feliz aniversario!
-          .md-subhead Esto es una gymkana online creada para ti
-    md-card-media-cover(md-solid)
-      md-card-media(md-ratio='16:9')
-        img(src='/dist/img/starwars.jpg')
-      md-card-area
-        md-card-header
-          .md-title Tal vez tengas que viajar
-          .md-subhead Tienes un coche eléctrico
-    md-card-media-cover(md-solid)
-      md-card-media(md-ratio='16:9')
-        img(src='/dist/img/lordoftherings.jpg')
-      md-card-area
-        md-card-header
-          .md-title Resolver acertijos
-          .md-subhead Eres más lista que yo
-    md-card-media-cover(md-solid)
-      md-card-media(md-ratio='16:9')
-        img(src='/dist/img/gameofthrones.jpg')
-      md-card-area
-        md-card-header
-          .md-title Luchar contra monstruos
-          .md-subhead O su equivalente legal
-    md-card-media-cover(md-solid)
-      md-card-media(md-ratio='16:9')
-        img(src='/dist/img/darkmatherials.jpg')
-      md-card-area
-        md-card-header
-          .md-title O encontrar tesoros
-          .md-subhead Tomar prestado no es robar
+          .md-title {{ card.title }}
+          .md-subhead {{ card.subtitle }}
 </template>
 
 <script>
+  export default {
+    data () {
+      return {
+        cards: [
+          {
+            title: 'Feliz aniversario!',
+            subtitle: 'Esto es una gymkana online creada para ti',
+            media: 'harrypotter.jpg'
+          },
+          {
+            title: 'Tal vez tengas que viajar',
+            subtitle: 'Tienes un coche eléctrico',
+            media: 'starwars.jpg'
+          },
+          {
+            title: 'Resolver acertijos',
+            subtitle: 'Descuida, eres más lista que yo',
+            media: 'lordoftherings.jpg'
+          },
+          {
+            title: 'Luchar contra monstruos',
+            subtitle: 'O lo más parecido a la realidad',
+            media: 'gameofthrones.jpg'
+          },
+          {
+            title: 'Hablar con NPCs',
+            subtitle: 'Como si de una misión se tratara',
+            media: 'wow.jpg'
+          },
+          {
+            title: 'O encontrar tesoros',
+            subtitle: 'Tomar prestado no es robar',
+            media: 'darkmatherials.jpg'
+          }
+        ]
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
